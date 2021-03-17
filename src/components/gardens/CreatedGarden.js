0486b1613c7e873412e0ec4e3c5c151f35e2ac2d
sllittrell <stacey.littrell@gmail.com>
2021-03-17 14:22:47 -0500

    updated GardenHome to only show current users gardens

--- src/components/gardens/CreatedGarden.js
+++ src/components/gardens/CreatedGarden.js
@@ -3,35 +3,29 @@ import { useHistory, useParams } from "react-router"
 import { GardenContext } from "./GardenProvider"
 
 export const CreatedGarden = () => {
-    const { getGardenById, getGardenType, gardenType} = useContext(GardenContext)
+    const { getGardenById, getGardenType, gardenType } = useContext(GardenContext)
 
     const[garden, setGarden] = useState({})
-    const[gardenT, setGardenT] =useState([])
+    const[gardenTypes, setGardenTypes] =useState({})
+  
 
     const {gardenId} = useParams()
     const history = useHistory()
-
+    
     useEffect(() => {
-        getGardenById(gardenId)
+    getGardenById(gardenId)
         .then((response) => {
             setGarden(response)
         })
-        .then(() =>{
-            getGardenType()
-            .then((response) => {
-                setGardenT(response)
-            })})
-        }, [])
-        console.log(gardenType)
-    const gardType = gardenType.find(type => type.id === parseInt(garden.gardenTypeId))
-    console.log(gardType)
+    }, [])
+    console.log(gardenType)
    return(
         <> 
             <h2>Hello</h2>
             <section>
                 <h3 className="created_gardenName">{garden.name}</h3>
                 <div className="created_gardenDate">Start Date: {garden.startDate}</div>
-                <div className="created_gardenType">Garden Type:</div>
+                <div className="created_gardenType">Garden Type:{}</div>
             </section>
         </>
     )
