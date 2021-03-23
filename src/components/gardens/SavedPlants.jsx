7e22a8ab594138ebff0bf47a17f371a6a398861f
sllittrell <stacey.littrell@gmail.com>
2021-03-23 09:08:22 -0500

    checkout

--- src/components/gardens/SavedPlants.jsx
+++ src/components/gardens/SavedPlants.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import {Link} from "react-router-dom"
+import {SavedPlantContext} from "../plants/SavedPlantProvider"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,12 +16,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const SavedPlantDividers = ({myPlants}) => {
+  const {removeSavedPlant}= useContext(SavedPlantContext)
   const classes = useStyles();
+
+  const handleRemove =() =>{
+    removeSavedPlant(myPlants?.id)
+  }
 
   return (
     <List component="nav" className={classes.root} aria-label="helpers">
-      <ListItem button to={`/plants/details/${myPlants?.id}`} component={Link}>
+      {/* <ListItem button to={`/plants/details/${myPlants?.id}`} component={Link}> */}
         <ListItemText primary={myPlants?.commonName} />  
+        <button onClick={handleRemove}>Remove Plant</button>
       </ListItem>
     </List>
   );
