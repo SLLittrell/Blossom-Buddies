505acc1c6f5e08987cd3766c03fe9b0bbaf97642
sllittrell <stacey.littrell@gmail.com>
2021-03-23 12:48:32 -0500

    user is able to see data do edit and change it successfully

--- src/components/gardens/GardenForm.js
+++ src/components/gardens/GardenForm.js
@@ -10,7 +10,7 @@ export const GardenForm = () => {
     const currentUserId = parseInt(sessionStorage.getItem(userStorageKey))
     
     const [isLoading, setIsLoading] = useState(true);
-    const [gardens, setGardens] =useState({})
+    // const [gardens, setGardens] =useState({})
     
     const [garden, setGarden] = useState({
         name:"",
@@ -41,9 +41,9 @@ export const GardenForm = () => {
                     userId: garden.userId,
                     startDate: garden.startDate,
                     gardenTypeId:parseInt(garden.gardenTypeId),
-                    id: gardens.id
+                    id: garden.id
                 })
-                .then(() => history.push(`/gardens/${gardens.id}`))
+                .then(() => history.push(`/gardens/${garden.id}`))
             }else {
                 addGarden({
                     name: garden.name,
@@ -64,7 +64,7 @@ export const GardenForm = () => {
             if (gardenId) {
               getGardenById(gardenId)
               .then(garden => {
-                  setGardens(garden)
+                  setGarden(garden)
                   setIsLoading(false)
                 })
               } else {
@@ -80,17 +80,17 @@ export const GardenForm = () => {
                 <fieldset>
                     <div className="garden_form">
                         <label htmlFor="name">Name: </label>
-                        <input type="text" id="name" value={gardens.name} autoFocus className="gardenInput" 
+                        <input type="text" id="name" value={garden.name} autoFocus className="gardenInput" 
                         onChange={inputChange}/>
                     </div>
               </fieldset>
               <fieldset>
                   <label htmlFor="startDate">Start Date:</label>
-                  <input type="date" id="startDate" className="gardenInput" defaultValue={gardens.startDate} onChange={inputChange}></input>
+                  <input type="date" id="startDate" className="gardenInput" value={garden.startDate} onChange={inputChange}></input>
               </fieldset>
               <fieldset>
                 <label htmlFor="gardenTypeId">Garden Type:</label>
-                <select id="gardenTypeId" defaultValue={gardens.gardenTypeId} onChange={inputChange}>
+                <select id="gardenTypeId" value={garden.gardenTypeId} onChange={inputChange}>
                     <option value= "0">Select a garden type</option>
                     {gardenType.map(types => (
                     <option key={types.id} value={types.id}>{types.type}</option>
