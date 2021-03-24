bcf13416fc5e7f6c94722b81fde6cb5451237faf
sllittrell <stacey.littrell@gmail.com>
2021-03-24 11:52:45 -0500

    comments unused parameter in garden home

--- src/components/gardens/GardenHome.js
+++ src/components/gardens/GardenHome.js
@@ -10,19 +10,21 @@ import {Link} from "react-router-dom"
 import { userStorageKey } from "../auth/authSettings";
 
 
-
+//pulls created garden data with useContext
 export const MyGardens = () => {
     const {gardens, getGardens} = useContext(GardenContext)
     
 
     const history =useHistory()
-    const {gardenId} = useParams()
+    
     const currentUserId = parseInt(sessionStorage.getItem(userStorageKey))
-
+    
+    //invokes getGarden fetch function on initial render
     useEffect(() => {
         getGardens()
     }, [])
    
+    //filters gardens by current user id
     const usersGarden = gardens.filter(garden => garden.userId === currentUserId)
 
     return (
