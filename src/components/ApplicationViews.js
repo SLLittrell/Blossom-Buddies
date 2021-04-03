b95823811a72dcaecfaf7ef13e0c8b5c6f28ddde
sllittrell <stacey.littrell@gmail.com>
2021-04-03 13:33:32 -0500

    deleted trefle api provider

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -11,7 +11,6 @@ import { PlantProvider } from "./plants/PlantProvider"
 import { UserProvider } from "./users/UserProvider"
 import {PlantSearch} from "./plants/PlantSearch"
 import {SavedPlantProvider} from "./plants/SavedPlantProvider"
-import { TreflePlantProvider } from "./plants/TreflePlantProvider"
 import { NoteProvider } from "./notes/NoteProvider"
 
 export const ApplicationViews = () => {
@@ -47,11 +46,9 @@ export const ApplicationViews = () => {
                 </Route>
                 <GardenProvider>
                     <SavedPlantProvider>
-                        <TreflePlantProvider>
                             <Route exact path="/plants/details/:plantId(\d+)">
                                 <PlantDetails />
                             </Route>
-                        </TreflePlantProvider>
                     </SavedPlantProvider>
                 </GardenProvider>
             </PlantProvider>
